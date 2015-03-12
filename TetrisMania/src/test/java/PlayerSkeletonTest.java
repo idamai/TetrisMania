3b3e089bdc1664cf3ec53197b575b19ecd78ffec
Joel Tong <me@joeltong.org>
2015-03-12 22:35:22 +0800

    added basic unit tests using junit4; ensured tests pass.  Use `mvn test` to verify

---
+++ TetrisMania/src/test/java/PlayerSkeletonTest.java
@@ -0,0 +1,23 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerSkeletonTest {
+
+  @Before
+  public void setUp() throws Exception {
+
+  }
+
+  @After
+  public void tearDown() throws Exception {
+
+  }
+
+  @Test
+  public void testPickMove() throws Exception {
+    System.out.println("This is a basic test");
+  }
+}
