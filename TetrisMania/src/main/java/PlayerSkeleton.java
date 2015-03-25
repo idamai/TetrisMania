f52c5a03d6bc3c44da59f44c143938a90aae75ee
nglinh <nglinh2312@gmail.com>
2015-03-25 12:01:29 +0800

    Added calculateHeuristic

--- TetrisMania/src/main/java/PlayerSkeleton.java
+++ TetrisMania/src/main/java/PlayerSkeleton.java
@@ -3,9 +3,25 @@ public class PlayerSkeleton {
 
 	//implement this function to have a working system
 	public int pickMove(State s, int[][] legalMoves) {
-		
+
 		return 0;
 	}
+
+
+    public double calculateFeature(double[] features){
+        return 0.0;
+    }
+
+    public double calculateHeuristic(double[] weights, State s){
+        double sum = 0;
+        double features[] = new double[22];
+        features[0] = 1;
+        calculateFeature(features);
+        for (int i = 0; i < weights.length; i++){
+            sum += weights[i]*features[i];
+        }
+        return sum;
+    }
 	
 	public static void main(String[] args) {
     //TODO: Add in proper logging library instead of System.out.println
