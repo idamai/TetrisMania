7245c8e9c1e27a982b7042ad05e9c0ea7293c795
Joel Tong <me@joeltong.org>
2015-04-18 11:34:52 +0800

    added working implementation of Master slave, inputs to be linked up

--- TetrisMania/src/test/java/MasterSlaveTest.java
+++ TetrisMania/src/test/java/MasterSlaveTest.java
@@ -6,25 +6,30 @@ import static org.junit.Assert.*;
 
 public class MasterSlaveTest {
   Master master;
-  Slave slave1;
+  Slave slave1,slave2;
 
   @Before
   public void setUp() throws Exception {
     master = new Master(9000);
     slave1 = new Slave("localhost", 9000, 9001);
-    master.start();
+    slave2 = new Slave("localhost", 9000, 9002);
     slave1.start();
+    slave2.start();
+    master.start();
   }
 
   @After
   public void tearDown() throws Exception {
     master.stop();
     slave1.stop();
+    slave2.stop();
   }
 
   @Test
   public void testProcess() throws Exception {
-    Core.sleep(5000);
+    Core.sleep(4000);
     master.display();
+    master.runSlaves();
+    Core.sleep(7000);
   }
 }
